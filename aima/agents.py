c0e26a0fd3affde5dd3e7226ae17a07c5476a540
ldbonequi <ldbonequi@gmail.com>
2026-09-04 13:57:38 -0600

    change model based agent to work in 2 dimensions

--- aima/agents.py
+++ aima/agents.py
@@ -265,20 +265,24 @@ def ModelBasedVacuumAgent():
     >>> environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
     True
     """
-    model = {loc_A: None, loc_B: None}
+    model = {loc_A: None, loc_B: None, loc_C: None, loc_D: None}
 
     def program(percept):
         """Same as ReflexVacuumAgent, except if everything is clean, do NoOp."""
         location, status = percept
         model[location] = status  # Update the model here
-        if model[loc_A] == model[loc_B] == 'Clean':
+        if model[loc_A] == model[loc_B] == model[loc_C] == model[loc_D] == 'Clean':
             return 'NoOp'
         elif status == 'Dirty':
             return 'Suck'
         elif location == loc_A:
             return 'Right'
         elif location == loc_B:
+            return 'Down'
+        elif location == loc_D:
             return 'Left'
+        elif location == loc_C:
+            return 'Up'
 
     return Agent(program)
 
